e57e938dd4998f8f83e5ebf2409e4dc8e8b77faf
Verathagnus <59093644+Verathagnus@users.noreply.github.com>
2023-09-24 20:38:48 +0530

    fix: dependency errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './style.css';
 import MedicalTopicsTable from './MTT';
-import syllabusData from '../public/syllabus.json';
 export default function App() {
   return (
     <div>
-      <MedicalTopicsTable medicalData={syllabusData} />
+      <MedicalTopicsTable />
     </div>
   );
 }
